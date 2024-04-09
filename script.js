7f4120439b457fc54407e081c4dc5e0b94b46f56
mia-chou <mia.fy.chou@hotmail.com>
2024-04-09 14:43:38 +0100

    Fixed code errors and added CSS styling

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const slides=document.querySelectorAll(".slides img");
-let slideIndex=0;
-let intervalID =null;
+// IMAGE SLIDER
 
-//initializeSlider();
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider(){
-
-  slides[slidesIndex].classList.add("displaySlide");
-
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
 }
- function showSlide(index){
 
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
 
- }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
 
- function prevSlide(){
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
 
- }
-
- function nextSlide(){
-
-
- }
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
